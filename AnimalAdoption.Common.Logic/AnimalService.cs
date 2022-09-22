0f3279d3ff538e4f1891aceff8bbb1064ee4e235
meister-joe <meister.joe@outlook.com>
2022-09-22 11:27:28 +0800

    updated as per exercise4

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,9 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Eragon", Age = 50, Description = "Soft natured" },
+            new Animal { Id = 2, Name = "Loki", Age = 50, Description = "Under a lot of pressure" },
+            new Animal { Id = 3, Name = "Alpha", Age = 50, Description = "Shiny and glasslike" },
         };
     }
 }
--- AnimalAdoption.Common.Logic/LoginService.cs
+++ AnimalAdoption.Common.Logic/LoginService.cs
@@ -41,7 +41,7 @@ namespace AnimalAdoption.Common.Logic
                     name = "Murphy";
                     break;
                 case "placeholder":
-                    name = "Placeholder";
+                    name = "Loki";
                     break;
             }
             return name;
